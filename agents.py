ab89f403f6b58733cde915d003265a64dfa5d6d8
Siddhesh Zalte <zaltesiddheshlsg@gmail.com>
2025-06-20 20:59:44 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,1 +1,39 @@
+from crewai import Agent
+from dotenv import load_dotenv
+load_dotenv()
+from tools import tool 
 
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+llm= ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+                            verbose=True,
+                            temperature=0.1,
+                            google_api_key=os.getenv("GOOGLE_API_KEY"))
+
+researcher=Agent(
+    role="Senior Researcher",
+    goal="Uncover ground breaking technologies in {topic}",
+    verbose=True,
+    memory=True,
+    backstory=(
+        "Driven by curiosty youre at the froe front of innovation, eager to explore and share knowledge that could change the world"
+    ),
+    tools=[],
+    llm=llm,
+    allow_delegtaion=True,
+)
+
+#creating writing agent with custom tools responsible in writing newes blog
+
+news_writer =Agent(
+    role="News Writer",
+    goal="Write a compelling news article about {topic}",
+    verbose=True,
+    memory=True,
+    backstory=(
+        "You are a seasoned journalist with a knack for storytelling, dedicated to delivering accurate and engaging news articles"
+    ),
+    tools=[tool],
+    llm=llm,
+    allow_delegation=False,
+)
